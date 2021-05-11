456ba6b56bb77cc0f8499399d179a2de67d44c81
Denise Anjos <deniseb.anjos@outlook.com>
2021-05-11 13:21:08 -0300

    feat(validation): validação de formulario de contato e nova publicação

--- frontend/blog/src/app/contact/contact.component.ts
+++ frontend/blog/src/app/contact/contact.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+   //Validação de formulario contato
+   nomeValido: boolean = false;
+   emailValido: boolean = false;
+   mensagemValida: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  enviarMensagem() {
+
+    if (!this.nomeValido || !this.emailValido || !this.mensagemValida) {
+      alert("Por gentileza, preencha todos os campos corretamente.")
+    } else if (this.nomeValido && this.emailValido && this.mensagemValida) {
+      alert('Mensagem enviada com sucesso!')
+    }
+  }
+
+    //Validação de campos formulario contato
+    validaNome(event: any){
+      this.nomeValido = this.validation(event.target.value.length < 3, event);
+    }
+  
+    validaEmail(event: any){
+      this.emailValido = this.validation(event.target.value.indexOf('@') == -1 || event.target.value.indexOf('.') == -1 || event.target.value.indexOf(' ') >= 1, event);
+    }
+  
+    validaMensagem(event: any){
+      this.mensagemValida = this.validation(event.target.value.length < 10, event)
+    }
+  
+    validation(condicao: boolean, event:any){
+      let valid = false;
+      if(condicao){
+        event.target.classList.remove("is-valid");
+        event.target.classList.add("is-invalid");
+      }else{
+        event.target.classList.remove("is-invalid");
+        event.target.classList.add("is-valid");
+        valid = true;
+      }
+      return valid;
+    }
 }
+
--- frontend/blog/src/app/feed/feed.component.ts
+++ frontend/blog/src/app/feed/feed.component.ts
@@ -12,6 +12,11 @@ export class FeedComponent implements OnInit {
   post: Post = new Post;
   tituloPost: string;
 
+  //Validação de dados para publicação
+  tituloValido: boolean = false;
+  nomeValido: boolean = false;
+  mensagemValida: boolean = false;
+
   constructor(private postService: PostService) { }
 
   ngOnInit() {
@@ -25,14 +30,17 @@ export class FeedComponent implements OnInit {
   }
 
   cadastrarMensagem() {
-    this.postService.postMensagem(this.post).subscribe((data: Post) => {
-      this.post = data
-      location.assign('/feed')
-    })
+    if (!this.nomeValido || !this.tituloValido || !this.mensagemValida) {
+      alert("Por gentileza preencha todos os campos corretamente.")
+    } else if (this.tituloValido && this.mensagemValida && this.nomeValido) {
+      this.postService.postMensagem(this.post).subscribe((data: Post) => {
+        this.post = data
+        location.assign('/feed')
+      })
+    }
   }
 
   findByTituloPostagem() {
-
     if(this.tituloPost == '') {
       this.findAllPosts()
     } else {
@@ -40,6 +48,33 @@ export class FeedComponent implements OnInit {
         this.listPost = resp;
       })
     }
+  }
+
+  //Validação de campos
+  validaNome(event: any){
+    this.nomeValido = this.validation(event.target.value.length < 3, event);
+  }
+
+  validaTitulo(event: any){
+    this.tituloValido = this.validation(event.target.value.length < 5, event);
+  }
+
+  validaMensagem(event: any){
+    this.mensagemValida = this.validation(event.target.value.length < 30, event)
+  }
+
+
+  validation(condicao: boolean, event:any){
+    let valid = false;
+    if(condicao){
+      event.target.classList.remove("is-valid");
+      event.target.classList.add("is-invalid");
+    }else{
+      event.target.classList.remove("is-invalid");
+      event.target.classList.add("is-valid");
+      valid = true;
+    }
+    return valid;
   }
 
 }
